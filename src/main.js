d9251e082b4739e6c8bd5bfa742b5d1bf1c30bf9
Lawted <3209571656@qq.com>
2023-01-07 20:48:08 +0800

    feat: add skt-detect

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ window.addEventListener("arjs-video-loaded", (e) => {
 const log = (msg) => {
   document.getElementById("log").innerHTML += `<br/> ${msg}`;
 };
+
 const initCanvas = () => {
   log("initCanvas...");
   let canvas = document.createElement("canvas");
@@ -15,119 +16,71 @@ const initCanvas = () => {
   const { width, height } = video.style;
   canvas.width = width.replace("px", "");
   canvas.height = height.replace("px", "");
+  video.width = width.replace("px", "");
+  video.height = height.replace("px", "");
   canvas.style.cssText = video.style.cssText;
   canvas.style.zIndex = 100;
   document.body.insertBefore(canvas, document.body.firstChild);
   log(`canvas: ${canvas.width} x ${canvas.height}`);
 };
 
-// const calSktline = (mask) => {}
-
-// console.log(cv)
-
-// const getSkyRegionGradient = (img, h, w) => {
-//   img_gray = cv.cvtColor()
-// }
-
-const HexRGB = {
-  "#5da8d0": [93, 168, 208],
-  "#000000": [0, 0, 0],
-};
-
-const registerColor = (color) => {
-  let registeredColor = [];
-  Object.keys(HexRGB).forEach((hex) => {
-    registeredColor.push(hex);
-    tracking.ColorTracker.registerColor(hex, function (r, g, b) {
-      const dx = r - HexRGB[hex][0];
-      const dy = g - HexRGB[hex][1];
-      const dz = b - HexRGB[hex][2];
-      return dx * dx + dy * dy + dz * dz < 1000;
-    });
-  });
-  return registeredColor;
-};
-
 const initTrack = () => {
   log("initTrack...");
-  let canvas = document.getElementById("canvas");
-  let context = canvas.getContext("2d");
-  let colors = registerColor();
-  // let tracker = new tracking.ColorTracker(colors);
-  // tracking.track("#arjs-video", tracker);
-  // tracker.on("track", function (event) {
-  //   context.clearRect(0, 0, canvas.width, canvas.height);
+  let video = document.getElementById('arjs-video')
+  let canvas = document.getElementById('canvas')
+  let cap = new cv.VideoCapture(video);
 
-  //   event.data.forEach(function (rect) {
-  //     if (rect.color === "custom") {
-  //       rect.color = tracker.customColor;
-  //     }
-
-  //     context.strokeStyle = rect.color;
-  //     context.strokeRect(
-  //       rect.x,
-  //       rect.y,
-  //       rect.width,
-  //       rect.height
-  //     );
-  //     context.font = "11px Helvetica";
-  //     context.fillStyle = "#fff";
-  //     context.fillText(
-  //       "x: " + rect.x + "px",
-  //       rect.x + rect.width + 5,
-  //       rect.y + 11
-  //     );
-  //     context.fillText(
-  //       "y: " + rect.y + "px",
-  //       rect.x + rect.width + 5,
-  //       rect.y + 22
-  //     );
-  //     context.fillText(
-  //       "color: " + rect.color,
-  //       rect.x + rect.width + 5,
-  //       rect.y + 33
-  //     );
-  //   });
-  // });
-  var MyTracker = function () {
-    MyTracker(this, "constructor");
-  };
-
-  MyTracker = function () {
-    MyTracker.prototype.track = function (pixels, width, height) {
-      let mainColor;
-      new ImgMainColor(
-        {
-          imageData: pixels,
-        },
-        function (color) {
-          mainColor = color;
-        }
-      );
-      this.emit("track", {
-        data: {
-          mainColor,
-        },
-      });
-    };
-  };
-
-  tracking.inherits(MyTracker, tracking.Tracker);
-
-  const myTracker = new MyTracker();
-
-  let lastColor;
-  const currentColor = document.getElementById("current-color");
-  const model = document.getElementById("model");
-  myTracker.on("track", function (event) {
-    // console log when color change
-    if (event.data.mainColor.hex !== lastColor) {
-      lastColor = event.data.mainColor.hex;
-      currentColor.style.backgroundColor = lastColor;
-      currentColor.innerHTML = lastColor;
-      model.attributes['light'].value = `type: ambient; color: ${lastColor}`;
+  const kSize = new cv.Size(9, 3);
+  const calSkyLine = (dst, h, w) => {
+    let skyline = cv.Mat.zeros(h, w, cv.CV_8UC1);
+    for (let j = 0; j < w; j++) {
+      for (let i = 0; i < h; i++) {
+        skyline.ucharPtr(i, j)[0] = 255;
+        if (dst.ucharPtr(i, j)[0] === 0) break;
+      }
     }
-  });
+    src.copyTo(dst, skyline);
+    skyline.delete();
+    return dst;
+  };
 
-  tracking.track("#arjs-video", myTracker);
+  const getSkyRegionGradient = (src, mask, h, w) => {
+    let imgGray = new cv.Mat();
+    cv.cvtColor(src, imgGray, cv.COLOR_RGBA2GRAY, 0);
+
+    cv.blur(imgGray, imgGray, kSize);
+    cv.medianBlur(imgGray, imgGray, 5);
+    let lap = new cv.Mat();
+    cv.Laplacian(imgGray, lap, cv.CV_8U, 1, 1, 0, cv.BORDER_DEFAULT);
+    // gradient_mask = (lap < 6).astype(np.uint8)
+    let gradient_mask = new cv.Mat();
+    cv.threshold(lap, gradient_mask, 6, 255, cv.THRESH_BINARY_INV);
+    let M = cv.Mat.ones(9, 3, cv.CV_8U);
+    cv.erode(gradient_mask, mask, M);
+    imgGray.delete();
+    lap.delete();
+    return calSkyLine(mask, h, w);
+  };
+
+  let src = new cv.Mat(canvas.height, canvas.width, cv.CV_8UC4);
+  let dst = new cv.Mat(canvas.height, canvas.width, cv.CV_8UC4);
+
+  const FPS = 30;
+  const processVideo = () => {
+    log(`start sky detect`)
+    try {
+      let begin = Date.now();
+      cap.read(src);
+      getSkyRegionGradient(src, dst, video.height, video.width);
+      cv.imshow("canvas", dst);
+      // schedule next one.
+      let delay = 1000 / FPS - (Date.now() - begin);
+      setTimeout(processVideo, delay);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  // schedule the first one.
+  setTimeout(processVideo, 1000);
 };
